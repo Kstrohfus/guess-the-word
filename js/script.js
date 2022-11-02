103e342e45e7a68ee365a1fadc4b9caac1c28b9a
Kstrohfus <houston11@me.com>
2022-11-02 16:56:55 -0700

    Added global variables to js starting place in script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const guessLetter = document.querySelector(".guessed-letters")
+const guessBtn = document.querySelector(".guess")
+const guessInput = document.querySelector(".letter")
+const wordProgress = document.querySelector(".word-in-progress")
+const remainGuessNum = document.querySelector(".remaining")
+const remainGuessText = document.querySelector("span .remaining")
+const remainMessage = document.querySelector(".message")
+const playAgainBtn = document.querySelector(".play-again hide")
+const word = ("magnolia")
+
+
