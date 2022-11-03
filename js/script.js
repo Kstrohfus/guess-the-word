0abde42f4b377a65ca41a1d3c8e6bfbd346e8b79
Kstrohfus <houston11@me.com>
2022-11-02 21:00:51 -0700

    Added dots placeholder, added event listener for letter input

--- js/script.js
+++ js/script.js
@@ -6,6 +6,30 @@ const remainGuessNum = document.querySelector(".remaining")
 const remainGuessText = document.querySelector("span .remaining")
 const remainMessage = document.querySelector(".message")
 const playAgainBtn = document.querySelector(".play-again hide")
-const word = ("magnolia")
+const word = "magnolia"
+
+//displaying dot inplace of letters
+const dotPlaceholder = function (word) {
+    const dots = [];
+    for (const letter of word) {
+        //console.log(letter)
+    dots.push("●");
+    }
+
+    wordProgress.innerText = dots.join("●")  
+      
+};
+
+dotPlaceholder(word);  
+
+//Listener for guess button
+
+guessBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guessedLetter = guessInput.value;
+console.log(guessedLetter);
+    guessInput.value = "";
+});
+
 
 
